40f133d3bbabd6f0a34c50cd519e4c70537d8da9
Christian Brice <brice.c.aa@m.titech.ac.jp>
2025-02-04 16:13:35 +0900

    working URDF

---
+++ ros2_ws/src/libra/launch/sensor_suite_test.launch.py
@@ -0,0 +1,109 @@
+import os
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.conditions import IfCondition, UnlessCondition
+from launch.substitutions import Command, LaunchConfiguration
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+from launch_ros.descriptions import ParameterValue
+
+
+def generate_launch_description():
+    # Set paths to required files and dirs
+    pkg_share = FindPackageShare(package='libra').find('libra')
+    default_rviz_config_path = os.path.join(
+        pkg_share, 'rviz/rviz_basic_settings.rviz')
+    default_urdf_model_path = os.path.join(
+        pkg_share, 'models/sensor_suite.urdf.xacro')
+
+    # --- DEFINE LAUNCH OPTIONS ---
+
+    urdf_model = LaunchConfiguration('urdf_model')
+    rviz_config_file = LaunchConfiguration('rviz_config_file')
+    use_gui = LaunchConfiguration('gui')
+    use_rviz = LaunchConfiguration('use_rviz')
+    use_sim_time = LaunchConfiguration('use_sim_time')
+
+    declare_urdf_model_path_cmd = DeclareLaunchArgument(
+        name='urdf_model',
+        default_value=default_urdf_model_path,
+        description='Full path to robot URDF file')
+
+    declare_rviz_config_file_cmd = DeclareLaunchArgument(
+        name='rviz_config_file',
+        default_value=default_rviz_config_path,
+        description='Full path to RViz config file')
+    
+    declare_use_joint_state_publisher_cmd = DeclareLaunchArgument(
+        name='use_gui',
+        default_value='True',
+        description='Whether to enable joint_state_publisher_gui')
+    
+    declare_use_rviz_cmd = DeclareLaunchArgument(
+        name='use_rviz',
+        default_value='True',
+        description='Whether to start RViz')
+
+    declare_use_sim_time_cmd = DeclareLaunchArgument(
+        name='use_sim_time',
+        default_value='False',
+        description='Whether to use simulation (Gazebo) clock')
+
+    # --- DEFINE ROS ACTIONS ---
+
+    # Publish joint state values for non-fixed joints (see URDF)
+    start_joint_state_publisher_cmd = Node(
+        condition=UnlessCondition(use_gui),
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
+        name='joint_state_publisher')
+    
+    # Manipulate joint state values via GUI
+    start_joint_state_publisher_gui_node = Node(
+        condition=IfCondition(use_gui),
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',
+        name='joint_state_publisher_gui')
+
+    # Subscribe to joint states of robot and publish 3D pose of each link
+    start_robot_state_publisher_cmd = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        parameters=[{'use_sim_time': use_sim_time,
+                     # Note: If ANY text in the xacro output can be interpreted as yaml,
+                     # the roslaunch system will try to interpret the ENTIRE text as yaml
+                     # instead of passing on the string. The biggest cause of this false
+                     # interpretation is commenting out xacro calls since the xacro:property
+                     # or similar looks a lot like a yaml key:value pair. Comments are not
+                     # removed by xacro so they are included in the output.
+                     # (source: https://answers.ros.org/question/417369/caught-exception-in-launch-see-debug-for-traceback-unable-to-parse-the-value-of-parameter-robot_description-as-yaml/)
+                     'robot_description': ParameterValue(Command(['xacro ', urdf_model]), value_type=str)}],
+        arguments=[default_urdf_model_path])
+
+    # Launch RViz
+    start_rviz_cmd = Node(
+        condition=IfCondition(use_rviz),
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        output='screen',
+        arguments=['-d', rviz_config_file])
+
+    # --- DECLARE LAUNCH OPTIONS ---
+
+    ld = LaunchDescription()
+
+    ld.add_action(declare_urdf_model_path_cmd)
+    ld.add_action(declare_rviz_config_file_cmd)
+    ld.add_action(declare_use_joint_state_publisher_cmd)
+    ld.add_action(declare_use_rviz_cmd)
+    ld.add_action(declare_use_sim_time_cmd)
+
+    # --- DECLARE ROS ACTIONS ---
+
+    ld.add_action(start_joint_state_publisher_cmd)
+    ld.add_action(start_joint_state_publisher_gui_node)
+    ld.add_action(start_robot_state_publisher_cmd)
+    ld.add_action(start_rviz_cmd)
+
+    return ld
