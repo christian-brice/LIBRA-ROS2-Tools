0a0dde0b63a75145c718c94853c0005410a7d56e
christian-brice <brice.c.67b9@m.isct.ac.jp>
2025-07-17 21:04:05 +0900

    updated stereo-only rtabmap

--- ros2_ws/src/libra/launch/rtabmap_stereo.launch.py
+++ ros2_ws/src/libra/launch/rtabmap_stereo.launch.py
@@ -1,3 +1,6 @@
+# Brief:
+#   Runs RTAB-Map in stereo mode with only RealSense data as input.
+#   (no robot odometry; visual-inertial odometry is used instead)
 # Requirements:
 #   An Intel RealSense D435i or D456
 #   Install the realsense2_camera ROS2 package (ros-$ROS_DISTRO-realsense2-camera)
@@ -9,77 +12,87 @@ import os
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node, SetParameter
+from launch_ros.substitutions import FindPackageShare
 
 def generate_launch_description():
-    # Configure nodes and DDS communications
-    parameters=[{
-          'frame_id':'camera_link',
-          'subscribe_stereo':True,
-          'subscribe_odom_info':True,
-          'wait_imu_to_init':True}]
+    # Set paths to required files and dirs
+    pkg_share = FindPackageShare('libra').find('libra')
+    config_path = os.path.join(pkg_share, 'config')
 
-    remappings=[
-          ('imu', '/imu/data'),
-          ('left/image_rect', '/camera/infra1/image_rect_raw'),
-          ('left/camera_info', '/camera/infra1/camera_info'),
-          ('right/image_rect', '/camera/infra2/image_rect_raw'),
-          ('right/camera_info', '/camera/infra2/camera_info')]
+    # Configure nodes and DDS communications
+    rtab_params=[{
+        'frame_id': 'camera_link',
+        'subscribe_stereo': True,
+        'subscribe_odom_info': True,
+        'wait_imu_to_init': True
+    }]
+
+    rtab_remaps=[
+        # Inputs: realsense2_camera
+        ('left/image_rect', '/camera/infra1/image_rect_raw'),
+        ('left/camera_info', '/camera/infra1/camera_info'),
+        ('right/image_rect', '/camera/infra2/image_rect_raw'),
+        ('right/camera_info', '/camera/infra2/camera_info'),
+        # Inputs: imu_filter_madgwick_node
+        ('imu', '/imu/data')
+    ]
 
     return LaunchDescription([
         # Launch arguments
         DeclareLaunchArgument(
-            'unite_imu_method', default_value='2',
-            description='0-None, 1-copy, 2-linear_interpolation. Use unite_imu_method:="1" if imu topics stop being published.'),
-
-        # Hack to disable IR emitter
-        SetParameter(name='depth_module.emitter_enabled', value=0),
+            'realsense_config',
+            default_value=os.path.join(config_path, 'realsense_stereo.yaml'),
+            description='Path to YAML file with RealSense camera parameters.'
+        ),
 
         # RealSense camera
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource([os.path.join(
                 get_package_share_directory('realsense2_camera'), 'launch'),
                 '/rs_launch.py']),
-                launch_arguments={
-                    'camera_namespace': '',
-                    'enable_color': 'false',
-                    'enable_depth': 'false',
-                    'enable_infra1': 'true',
-                    'enable_infra2': 'true',
-                    'enable_gyro': 'true',
-                    'enable_accel': 'true',
-                    'unite_imu_method': LaunchConfiguration('unite_imu_method'),
-                    'enable_sync': 'true'
-                }.items()),
+            launch_arguments={
+                'camera_namespace': '',  # avoid nesting "/camera/camera"
+                'config_file': LaunchConfiguration('realsense_config')
+            }.items()
+        ),
 
         # Compute quaternion of the IMU
         Node(
-            package='imu_filter_madgwick', executable='imu_filter_madgwick_node',output='screen',
-            parameters=[{'use_mag': False, 
-                         'world_frame': 'enu', 
-                         'publish_tf': False}],
-            remappings=[('imu/data_raw', '/camera/imu')]),
+            package='imu_filter_madgwick', executable='imu_filter_madgwick_node', output='screen',
+            parameters=[{
+                'use_mag': False,
+                'world_frame': 'enu',
+                'publish_tf': False
+            }],
+            remappings=[
+                # Inputs: realsense2_camera
+                ('imu/data_raw', '/camera/imu')
+            ]
+        ),
 
         # Compute odometry using stereo images
         Node(
             package='rtabmap_odom', executable='stereo_odometry', output='screen',
-            parameters=parameters,
-            remappings=remappings),
+            parameters=rtab_params,
+            remappings=rtab_remaps
+        ),
 
         # Primary RTAB-Map SLAM computation
         Node(
             package='rtabmap_slam', executable='rtabmap', output='screen',
-            parameters=parameters,
-            remappings=remappings,
-            arguments=['-d']),
+            parameters=rtab_params,
+            remappings=rtab_remaps,
+            arguments=['-d']  # delete previous database on launch (DEBUG ONLY)
+        ),
 
         # RTAB-Map's custom RViz GUI
         Node(
             package='rtabmap_viz', executable='rtabmap_viz', output='screen',
-            parameters=parameters,
-            remappings=remappings),
+            parameters=rtab_params,
+            remappings=rtab_remaps
+        ),
     ])
